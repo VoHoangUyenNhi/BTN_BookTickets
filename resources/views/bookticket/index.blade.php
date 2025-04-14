38958490e80a93699e6b718d62884a8526945fa7
Thu-Thuy-tttt <ttthutttthuy@gmail.com>
2025-04-14 21:21:16 +0700

    cập nhật css giao diện index

--- resources/views/bookticket/index.blade.php
+++ resources/views/bookticket/index.blade.php
@@ -5,44 +5,38 @@
 
         <div class="search-container">
             <div class="search-bar">
-                <!-- Điểm đi -->
-                <div class="form-group">
-                    <label for="diemdi">Điểm đi:</label>
-                    <select class="form-control" id="diemdi" name="DiemDi">
-                        <option value="">-- Chọn điểm đi --</option>
-                        @foreach ($locations as $location)
-                            <option value="{{ $location }}" {{ request('DiemDi') == $location ? 'selected' : '' }}>
-                                {{ $location }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+            <!-- Điểm đi -->
+            <div class="form-group">
+                <label for="diemdi">Điểm đi:</label>
+                <select class="form-control" id="diemdi" name="DiemDi" required>
+                    <option value="">-- Chọn điểm đi --</option>
+                    @foreach ($locations as $location)
+                        <option value="{{ $location }}" {{ request('DiemDi') == $location ? 'selected' : '' }}>
+                            {{ $location }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
 
-                <!-- Điểm đến -->
-                <div class="form-group">
-                    <label for="diemden">Điểm đến:</label>
-                    <select class="form-control" id="diemden" name="DiemDen">
-                        <option value="">-- Chọn điểm đến --</option>
-                        @foreach ($locations as $location)
-                            <option value="{{ $location }}" {{ request('DiemDen') == $location ? 'selected' : '' }}>
-                                {{ $location }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                <!-- Ngày đi -->
-                <div class="form-group">
-                    <label for="ngaydi">Ngày đi</label>
-                    <input id="ngaydi" name="ThoiGianKhoiHanh" type="date" class="form-control"
-                        value="{{ old('ThoiGianKhoiHanh', request('ThoiGianKhoiHanh')) }}">
-                </div> 
+            <!-- Điểm đến -->
+            <div class="form-group">
+                <label for="diemden">Điểm đến:</label>
+                <select class="form-control" id="diemden" name="DiemDen" required>
+                    <option value="">-- Chọn điểm đến --</option>
+                    @foreach ($locations as $location)
+                        <option value="{{ $location }}" {{ request('DiemDen') == $location ? 'selected' : '' }}>
+                            {{ $location }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
 
-                <!-- Ngày về -->
-                <div class="form-group" id="oNgayVe" style="display: none;">
-                    <label for="ngayve">Ngày về</label>
-                    <input id="ngayve" name="ngayve" type="date" class="form-control"
-                        value="{{ old('ngayve', request('ngayve')) }}">
-                </div>         
+            <!-- Ngày đi -->
+            <div class="form-group">
+                <label for="ngaydi">Ngày đi</label>
+                <input id="ngaydi" name="ThoiGianKhoiHanh" type="date" class="form-control"
+                    value="{{ old('ThoiGianKhoiHanh', request('ThoiGianKhoiHanh')) }}" required>
+            </div>       
                 <!-- Nút tìm -->
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Tìm chuyến</button>
